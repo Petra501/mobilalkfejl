5b6fed32d1dc1660c00449da715f5b17e969f52a
Sötét Petra <h164941@stud.u-szeged.hu>
2024-04-05 17:17:59 +0200

    register activity input adatok lekérése és eltárolása

--- app/src/main/java/com/example/konyvesbolt/RegisterActivity.java
+++ app/src/main/java/com/example/konyvesbolt/RegisterActivity.java
@@ -3,9 +3,17 @@ package com.example.konyvesbolt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText passwordAgainEditText;
+    EditText phoneEditText;
+    EditText typeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,13 @@ public class RegisterActivity extends AppCompatActivity {
         if(key != 44){
             finish();
         }
+
+        userNameEditText = findViewById(R.id.userNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordAgainEditText = findViewById(R.id.passwordAgainEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        typeEditText = findViewById(R.id.typeEditText);
     }
 
     public void cancel(View view) {
@@ -24,5 +39,18 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String userEmail = userEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordAgain = passwordAgainEditText.getText().toString();
+        String phone = phoneEditText.getText().toString();
+        String type = typeEditText.getText().toString();
+
+        if (!password.equals(passwordAgain)){
+            Log.e("RegisterActivity", "A jelszó és megerősítése nem egyezik!");
+        }
+
+        Log.i("RegisterActivity", "Regisztrált: " + userName + ", Email: " + userEmail + ", telefon: " + phone + ", típus: " + type);
+        //TODO: A regisztrációs funkcionalitást meg kellene valósítani egyszer.
     }
 }
