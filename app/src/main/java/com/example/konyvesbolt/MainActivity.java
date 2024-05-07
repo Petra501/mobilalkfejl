3cd28253e71b2f43b967dda87da8e99fbfc9d952
Sötét Petra <h164941@stud.u-szeged.hu>
2024-05-07 15:32:09 +0200

    telefonszám típusának választhatósága

--- app/src/main/java/com/example/konyvesbolt/MainActivity.java
+++ app/src/main/java/com/example/konyvesbolt/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.konyvesbolt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getName();
+    private static final String CLASS = MainActivity.class.getName();
+    private static final String PREFERENCE = MainActivity.class.getPackage().toString();
+    private SharedPreferences preferences;
     private static final int KEY = 44;
 
     EditText userNameEditText;
     EditText passwordEditText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         userNameEditText = findViewById(R.id.editTextUserName);
         passwordEditText = findViewById(R.id.editTextPassword);
+
+        preferences = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
+
+        Log.i(CLASS, "onCreate");
     }
 
 
@@ -29,13 +38,55 @@ public class MainActivity extends AppCompatActivity {
         String userName = userNameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        Log.i("MainActivity", "Felhasználónév: " + userName + ", jelszó: " + password);
+        Log.i(CLASS, "Felhasználónév: " + userName + ", jelszó: " + password);
     }
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
-        intent.putExtra("KEY", 44);
+        intent.putExtra("KEY", KEY);
         //TODO
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CLASS, "onStart");
+        //TODO: Az onStart és egyéb life ciklusos logolásait ki kell szedni
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(CLASS, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(CLASS, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userNameEditText.getText().toString());
+        editor.putString("password", passwordEditText.getText().toString());
+        editor.apply();
+
+        Log.i(CLASS, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(CLASS, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(CLASS, "onRestart");
     }
 }
--- app/src/main/java/com/example/konyvesbolt/RegisterActivity.java
+++ app/src/main/java/com/example/konyvesbolt/RegisterActivity.java
@@ -2,18 +2,27 @@ package com.example.konyvesbolt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String CLASS = RegisterActivity.class.getName();
+    private static final String PREFERENCE = RegisterActivity.class.getPackage().toString();
+    private SharedPreferences preferences;
+
     EditText userNameEditText;
     EditText userEmailEditText;
     EditText passwordEditText;
     EditText passwordAgainEditText;
     EditText phoneEditText;
-    EditText typeEditText;
+   // EditText typeEditText;
+    Spinner phoneSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +40,24 @@ public class RegisterActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordAgainEditText = findViewById(R.id.passwordAgainEditText);
         phoneEditText = findViewById(R.id.phoneEditText);
-        typeEditText = findViewById(R.id.typeEditText);
+        phoneSpinner = findViewById(R.id.phoneSpinner);
+        //typeEditText = findViewById(R.id.typeEditText);
+
+        preferences = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameEditText.setText(userName);
+        passwordEditText.setText(password);
+        passwordAgainEditText.setText(password);
+
+        phoneSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter =  ArrayAdapter.createFromResource(this, R.array.phoneType, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        phoneSpinner.setAdapter(adapter);
+
+        Log.i(CLASS, "onCreate");
     }
 
     public void cancel(View view) {
@@ -43,14 +69,66 @@ public class RegisterActivity extends AppCompatActivity {
         String userEmail = userEmailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
         String passwordAgain = passwordAgainEditText.getText().toString();
+
         String phone = phoneEditText.getText().toString();
-        String type = typeEditText.getText().toString();
+        String phoneType = phoneSpinner.getSelectedItem().toString();
+
+        //String type = typeEditText.getText().toString();
 
         if (!password.equals(passwordAgain)){
-            Log.e("RegisterActivity", "A jelszó és megerősítése nem egyezik!");
+            Log.e(CLASS, "A jelszó és megerősítése nem egyezik!");
+            return;
         }
 
-        Log.i("RegisterActivity", "Regisztrált: " + userName + ", Email: " + userEmail + ", telefon: " + phone + ", típus: " + type);
+
+       // Log.i(CLASS, "Regisztrált: " + userName + ", Email: " + userEmail + ", telefon: " + phone + ", típus: " + type);
         //TODO: A regisztrációs funkcionalitást meg kellene valósítani egyszer.
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CLASS, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(CLASS, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(CLASS, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(CLASS, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(CLASS, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(CLASS, "onRestart");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selectedPhoneItem = parent.getItemAtPosition(position).toString();
+        Log.i(CLASS, selectedPhoneItem);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //TODO: kiválaszott spiner item üres
     }
 }
